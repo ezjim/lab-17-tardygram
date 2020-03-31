0abffe7b5097ab90cb01d1cb60c35c229e28b3e6
james eserjose <james.re@yahoo.com>
2020-03-31 15:53:50 -0700

    seed data done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,20 +1,22 @@
-require('dotenv').config();
+const { getUser, getAgent } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
 
-describe('app routes', () => {
-  beforeAll(() => {
-    connect();
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  afterAll(() => {
-    return mongoose.connection.close();
+describe('auth routes', () => {
+  it('signs up a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'spot@dogs.com',
+        password: 'spotWasHere'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          email: 'spot@dogs.com',
+          __v: 0
+        });
+      });
   });
 });
