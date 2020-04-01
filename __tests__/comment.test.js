dadecb32a5f740da791545d758ea68d29b795e11
james eserjose <james.re@yahoo.com>
2020-04-01 15:57:10 -0700

    updating comments

--- __tests__/comment.test.js
+++ __tests__/comment.test.js
@@ -7,34 +7,18 @@ describe('comments routes', () => {
     return getAgent()
       .post('/api/v1/comments')
       .send({
-        comment: 'my title',
-        body: 'my body'
+        comment: 'my comment',
+        post: insta._id
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          title: 'my title',
-          body: 'my body',
-          author: user._id,
+          commentBy: user._id,
+          comment: 'my comment',
+          post: insta._id,
           createdAt: expect.any(String),
           updatedAt: expect.any(String),
           __v: 0
-        });
-      });
-  });
-  
-  it('updates a comment', async() => {
-    const user = await getUser({ username: 'jimmy' });
-    const comment = await getComment({ author: user._id });
-
-    return getAgent()
-      .patch(`/api/v1/comments/${comment._id}`)
-      .send({ title: 'My cool comment' })
-      .then(res => {
-        expect(res.body).toEqual({
-          ...comment,
-          updatedAt: expect.any(String),
-          title: 'My cool comment'
         });
       });
   });
